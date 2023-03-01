a00a9de5dcb644ab58ec1cf04107bb9ad27dc8ab
amorags <115067025+amorags@users.noreply.github.com>
2023-03-01 12:12:36 +0100

    Buttons

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -1,7 +1,11 @@
 package rps.gui.controller;
 
 // Java imports
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -10,6 +14,32 @@ import java.util.ResourceBundle;
  * @author smsj
  */
 public class GameViewController implements Initializable {
+
+    @FXML
+    private Button btnPaper;
+
+    @FXML
+    private Button btnRock;
+
+    @FXML
+    private Button btnSci;
+
+    @FXML
+    void handlePaper(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRock(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSci(ActionEvent event) {
+
+    }
+
+
 
     /**
      * Initializes the controller class.
